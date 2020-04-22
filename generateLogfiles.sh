defdd716799459067150f6d0065b0ace7c389411
Markus Stroh <markusstroh@Markuss-MacBook-Pro.local>
2020-04-22 11:00:54 +0200

    Some cleanup

--- generateLogfiles.sh
+++ generateLogfiles.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
 # hier muss noch rein, dass man start und ende über das terminal übergibt
-
-i=24
-while [ $i -le  54 ]
+sessionCnt=0
+i=0 # Anzahl tage
+while [ $i -le  31 ]
 do
-	j=1
+	j=1  # Anzahl der Sessions an einem Tag
 	while [ $j -le 11 ]
 	do
-		if [ $(($i % 3)) -eq 0 ]
+		if [ $(($j % 3)) -eq 0 ]
 		then
-			python3.7 logEntryGenerator.py -d $i -f
+			python3.7 python/logEntryGenerator.py -d $i -f
+			sessionCnt=$(($sessionCnt +1))
 		else
-			python3.7 logEntryGenerator.py -d $i
+			python3.7 python/logEntryGenerator.py -d $i
+			sessionCnt=$(($sessionCnt +1))
 		fi
 		j=$(($j +1))
 	done
 	i=$(($i +1))
 done
 
+echo "$sessionCnt sessions generated"
 
 
 #i=1
