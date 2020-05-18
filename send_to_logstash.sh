0cc517c425b9a5242e10fa375aedf73a65a39b83
Markus Stroh <markusstroh@Markuss-MacBook-Pro.local>
2020-05-18 19:51:41 +0200

    corrected some minor mistakes

--- send_to_logstash.sh
+++ send_to_logstash.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# This script can be used to send queries to elasticsearch. The first argument should be the index that is going to be searched, the second shpuld be a .json
+# file containing the search query
 curl -X POST -H "Content-Type: application/json" -d @$1 http://localhost:9200/$2/_search?pretty
 
 
